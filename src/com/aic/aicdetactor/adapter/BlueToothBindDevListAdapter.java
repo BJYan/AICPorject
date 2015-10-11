0c1b1b87d782206871023a472fb3ff6165c255ef
gaven <gaven_lt@foxmail.com>
2015-10-11 21:42:55 +0800

    gaven:modify route item color
    Signed-off-by: gaven <gaven_lt@foxmail.com>

---
+++ src/com/aic/aicdetactor/adapter/BlueToothBindDevListAdapter.java
@@ -0,0 +1,83 @@
+package com.aic.aicdetactor.adapter;
+
+import com.aic.aicdetactor.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+public class BlueToothBindDevListAdapter extends BaseExpandableListAdapter {
+	Context context;
+	private LayoutInflater mInflater;
+
+	public BlueToothBindDevListAdapter(Context context) {
+		// TODO Auto-generated constructor stub
+		this.context = context;
+		mInflater = LayoutInflater.from(context);
+	}
+	
+	@Override
+	public Object getChild(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getChildId(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getChildView(int arg0, int arg1, boolean arg2, View arg3, ViewGroup arg4) {
+		// TODO Auto-generated method stub
+		if(arg3==null) arg3 = mInflater.inflate(R.layout.bluetooth_binded_devlist_child_item, null);
+		return arg3;
+	}
+
+	@Override
+	public int getChildrenCount(int arg0) {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+	@Override
+	public Object getGroup(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getGroupCount() {
+		// TODO Auto-generated method stub
+		return 5;
+	}
+
+	@Override
+	public long getGroupId(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getGroupView(int arg0, boolean arg1, View arg2, ViewGroup arg3) {
+		// TODO Auto-generated method stub
+		if(arg2==null) arg2 = mInflater.inflate(R.layout.bluetooth_binded_devlist_group_item, null);
+		return arg2;
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isChildSelectable(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
