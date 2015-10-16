059141c170deeb2e80dbd7d42039f10ece0af232
gaven <gaven_lt@foxmail.com>
2015-10-16 22:59:41 +0800

    gaven:modify chart dialog
    Signed-off-by: gaven <gaven_lt@foxmail.com>

--- src/com/aic/aicdetactor/dialog/SuperChartDialog.java
+++ src/com/aic/aicdetactor/dialog/SuperChartDialog.java
@@ -8,6 +8,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.WindowManager.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TabHost;
@@ -34,64 +35,50 @@ public class SuperChartDialog extends Dialog{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.dialog_super_chart_layout);
 		
-		setController("时域","轴心","频域");
-		setController("x","y","z");
-		controllerInit();
+		String[] tab_first = new String[]{"时域","轴心","频域"};
+		String[] tab_sec = new String[]{"x","y","z"};
+		controllerInit(tab_first,tab_sec);
 	}
-
-	private void controllerInit() {
-		
-		ctrlerContainer = (LinearLayout) findViewById(R.id.dialog_controller_container);
-		ctrlerContainer.removeAllViews();
-		for(int i=0;i<ctrlerContainerList.size();i++){
-			if(i==0&&ctrlerContainerList.get(0)!=null) {
-				LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-					    LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-				params.weight = 1.0f;
-				ctrlerContainerList.get(0).setLayoutParams(params);
-				ctrlerContainer.addView(ctrlerContainerList.get(0));
-			} else if(ctrlerContainerList.get(i)!=null) {
-				LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-					    LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-				params.weight = 1.0f;
-				params.leftMargin = 10;
-				ctrlerContainerList.get(i).setLayoutParams(params);
-				ctrlerContainer.addView(ctrlerContainerList.get(i));
-			}
+	
+	private void controllerInit(String[] tabname1,String[] tabname2){
+		if(tabname1!=null) {
+			TabHost tab1 = (TabHost)findViewById(R.id.dialog_tabhost1);
+			tab1.setVisibility(View.VISIBLE);
+			TabViewInit(tab1, tabname1);
+		}
+		if(tabname2!=null) {
+			TabHost tab2 = (TabHost)findViewById(R.id.dialog_tabhost2);
+			tab2.setVisibility(View.VISIBLE);
+			TabViewInit(tab2, tabname2);
+		}
+		if(tabname1!=null&&tabname2!=null){
+			View line = findViewById(R.id.dialog_tabhost_line);
+			line.setVisibility(View.VISIBLE);
 		}
 	}
 	
-	protected TabHost getTabView(String tabname1, String tabname2, String tabname3){
-		TabHost tab = (TabHost) inflater.inflate(R.layout.dialog_tabhost_layout, null);
+	protected TabHost TabViewInit(TabHost tab, String[] tabname){
 		tab.setup();
         
 		TabSpec tab1 = tab.newTabSpec("tab1");
 		TextView tab1_tv = (TextView) inflater.inflate(R.layout.dialog_tab_layout, null);
-		if(tabname1!=null) tab1_tv.setText(tabname1);
+		if(tabname[0]!=null) tab1_tv.setText(tabname[0]);
 		tab1.setIndicator(tab1_tv);
 		tab1.setContent(R.id.tab1);
 		tab.addTab(tab1);
 		TabSpec tab2 = tab.newTabSpec("tab2");
 		TextView tab2_tv = (TextView) inflater.inflate(R.layout.dialog_tab_layout, null);
-		if(tabname2!=null) tab2_tv.setText(tabname2);
+		if(tabname[1]!=null) tab2_tv.setText(tabname[1]);
 		tab2.setIndicator(tab2_tv);
 		tab2.setContent(R.id.tab2);
 		tab.addTab(tab2);
 		TabSpec tab3 = tab.newTabSpec("tab3");
 		TextView tab3_tv = (TextView) inflater.inflate(R.layout.dialog_tab_layout, null);
-		if(tabname3!=null) tab3_tv.setText(tabname3);
+		if(tabname[2]!=null) tab3_tv.setText(tabname[2]);
 		tab3.setIndicator(tab3_tv);
 		tab3.setContent(R.id.tab3);
 		tab.addTab(tab3);
 		return tab;
 	}
-	
-	protected void setController(String tabname1, String tabname2, String tabname3){
-		TabHost controller = getTabView(tabname1, tabname2, tabname3);
-		ctrlerContainerList.add(controller);
-	}
 
-	protected void clearController(){
-		ctrlerContainerList.clear();
-	}
 }
