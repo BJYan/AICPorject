802b86f44daaa950ccb5005d5c2534a6f663278c
gaven <gaven_lt@foxmail.com>
2015-10-24 20:44:53 +0800

    gaven:modify search popupwindow
    Signed-off-by: gaven <gaven_lt@foxmail.com>

--- src/com/aic/aicdetactor/check/SearchResultActivity.java
+++ src/com/aic/aicdetactor/check/SearchResultActivity.java
@@ -4,10 +4,13 @@ import com.aic.aicdetactor.CommonActivity;
 import com.aic.aicdetactor.R;
 import com.aic.aicdetactor.adapter.SearchResultStationExListAdapter;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.WindowManager.LayoutParams;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
@@ -16,6 +19,7 @@ import android.widget.PopupWindow;
 public class SearchResultActivity extends CommonActivity implements OnClickListener{
 	ImageView SerachBtn;
 	LayoutInflater inflater;
+	View content;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +28,8 @@ public class SearchResultActivity extends CommonActivity implements OnClickListe
 		
 		setActionBar("本地查询",true);
 		inflater = getLayoutInflater();
-		setContentView(R.layout.search_result_layout);
+		content = inflater.inflate(R.layout.search_result_layout, null);
+		setContentView(content);
 		ExpandableListView localSearchExListView = (ExpandableListView) findViewById(R.id.local_search_listview);
 		SearchResultStationExListAdapter localSearchExListAdapter = new SearchResultStationExListAdapter(getApplicationContext());
 		localSearchExListView.setAdapter(localSearchExListAdapter);
@@ -39,8 +44,20 @@ public class SearchResultActivity extends CommonActivity implements OnClickListe
 		switch (arg0.getId()) {
 		case R.id.search_button:
 			View searchPopWinView = inflater.inflate(R.layout.search_database_item_routemodel, null);
-			PopupWindow searchPopWin = new PopupWindow(searchPopWinView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-			searchPopWin.showAsDropDown(arg0);
+			final PopupWindow searchPopWin = new PopupWindow(searchPopWinView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT,true);
+			searchPopWin.setBackgroundDrawable(new BitmapDrawable());
+			if(searchPopWin!=null&&!searchPopWin.isShowing()) searchPopWin.showAsDropDown(arg0);
+			content.setOnTouchListener(new OnTouchListener() {
+				
+				@Override
+				public boolean onTouch(View arg0, MotionEvent arg1) {
+					// TODO Auto-generated method stub
+					if(searchPopWin!=null&&searchPopWin.isShowing()) {
+						searchPopWin.dismiss();
+					}
+					return false;
+				}
+			});
 			break;
 
 		default:
--- src/com/aic/aicdetactor/check/SearchResultConActivity.java
+++ src/com/aic/aicdetactor/check/SearchResultConActivity.java
@@ -5,10 +5,13 @@ import com.aic.aicdetactor.R;
 import com.aic.aicdetactor.adapter.SearchResultConExListAdapter;
 
 import android.app.Activity;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.WindowManager.LayoutParams;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
@@ -18,15 +21,17 @@ public class SearchResultConActivity extends CommonActivity implements OnClickLi
 	ExpandableListView exList;
 	ImageView SerachBtn;
 	LayoutInflater inflater; 
+	View content;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.search_condition_result_layout);
+		inflater = getLayoutInflater();
+		content = inflater.inflate(R.layout.search_condition_result_layout, null);
+		setContentView(content);
 		
 		setActionBar("数据库查询",true);
-		inflater = getLayoutInflater();
 		exList = (ExpandableListView) findViewById(R.id.search_con_result_list);
 		SearchResultConExListAdapter searchResConExListAdapter = new SearchResultConExListAdapter(getApplicationContext());
 		exList.setAdapter(searchResConExListAdapter);
@@ -41,8 +46,20 @@ public class SearchResultConActivity extends CommonActivity implements OnClickLi
 		switch (arg0.getId()) {
 		case R.id.search_con_button: 
 			View searchPopWinView = inflater.inflate(R.layout.search_database_item_searchmodel, null);
-			PopupWindow searchPopWin = new PopupWindow(searchPopWinView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-			searchPopWin.showAsDropDown(arg0);
+			final PopupWindow searchPopWin = new PopupWindow(searchPopWinView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT,true);
+			searchPopWin.setBackgroundDrawable(new BitmapDrawable());
+			if(searchPopWin!=null&&!searchPopWin.isShowing()) searchPopWin.showAsDropDown(arg0);
+			content.setOnTouchListener(new OnTouchListener() {
+				
+				@Override
+				public boolean onTouch(View arg0, MotionEvent arg1) {
+					// TODO Auto-generated method stub
+					if(searchPopWin!=null&&searchPopWin.isShowing()) {
+						searchPopWin.dismiss();
+					}
+					return false;
+				}
+			});
 			break;
 
 		default:
